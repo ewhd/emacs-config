b8f31970af1a8707fe19ea5910ed1a57b4536ee9
ewhd <ewhd22@gmail.com>
2021-10-29 17:35:42 -0700

    installed magit and updated emac-config.org and init.el

--- init.el
+++ init.el
@@ -6,6 +6,25 @@
 (add-to-list 'package-archives 
   '("melpa" . "http://melpa.org/packages/"))
 
+;; The purpose of this file is to redirect emacs to look for its 'init' file 
+;; under the Home folder of a Windows 10 machine and to recognize its org-mode 
+;; formatting.
+;;
+;; Windows 10 will place .emacs.d under "C:/Users/USER/AppData/Roaming", UNLESS
+;; the HOME directory is set, so do that first and set it to "C:\Users\USER\"
+;; 
+;; This file should be located at "C:\Users\USER\.emacs.d\init.el", along with 
+;; the "~/.emacs.d/emacs-config.org" file it refers to.
+;; 
+;; First, package stuff is initialized, and use-package is installed if it 
+;; is not alread.
+;; Then org is required.
+;; Then emacs is instructed to load "~/.emacs.d/emacs-config.org" as its 'init' 
+;; file.
+;; The rest is for internal use.
+;; 
+;;
+
 (unless package-archive-contents
   (package-refresh-contents))
 
@@ -25,15 +44,21 @@
 ;; load org package and our emacs-config.org file
 (require 'org)
 (org-babel-load-file "~/.emacs.d/emacs-config.org") 
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
+ '(custom-safe-themes
+   '("9685cefcb4efd32520b899a34925c476e7920725c8d1f660e7336f37d6d95764" default))
  '(inhibit-startup-screen t)
+ '(initial-buffer-choice "~/Dropbox/gtd/gtd.org")
  '(line-spacing 0.0)
- '(package-selected-packages '(evil markdown-mode helm-org-rifle use-package)))
+ '(package-selected-packages
+   '(magit olivetti org-superstar org-appear company-posframe mixed-pitch org-beautify-theme evil markdown-mode helm-org-rifle use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
