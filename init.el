dd4074581df789ed9fd0e75b77a542602a4033c3
ewhd <ewhd22@gmail.com>
2022-03-14 01:49:06 -0700

    made a bunch of changes to emacs-config.org over the last few months

--- init.el
+++ init.el
@@ -58,10 +58,12 @@
  '(initial-buffer-choice "~/Dropbox/gtd/gtd.org")
  '(line-spacing 0.0)
  '(package-selected-packages
-   '(magit olivetti org-superstar org-appear company-posframe mixed-pitch org-beautify-theme evil markdown-mode helm-org-rifle use-package)))
+   '(adaptive-wrap highlight-parentheses magit olivetti org-superstar org-appear company-posframe mixed-pitch org-beautify-theme evil markdown-mode helm-org-rifle use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
